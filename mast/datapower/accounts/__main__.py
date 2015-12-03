7b04c9211aeaeb133cadb3f97b0c5c4be53a0d72
ilovetux <me@ilovetux.com>
2015-12-03 16:48:28 -0500

    Improved error handling

--- mast/datapower/accounts/__main__.py
+++ mast/datapower/accounts/__main__.py
@@ -4,4 +4,9 @@ from accounts import cli
 import sys
 sys.argv[0] = "mast-accounts"
 
-cli.Run()
+try:
+    cli.Run()
+except AttributeError, e:
+    if "'NoneType' object has no attribute 'app'" in e:
+        raise NotImplementedError(
+            "HTML formatted output is not supported on the CLI")
